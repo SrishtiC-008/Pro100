3f281efd11a0daf8b925aac4865c66ca455b8b1c
SrishtiC-008 <84587767+SrishtiC-008@users.noreply.github.com>
2021-09-21 15:45:50 +0530

    Add files via upload

---
+++ Pro 100/atm.py
@@ -0,0 +1,10 @@
+class Atm(object):
+    def __init__(self, cardNumber, pin):
+        self.cardNumber = cardNumber
+        self.pin = pin
+
+    def cashWithdrawal(self,cardNumber,pin):
+        print("") 
+        
+    def bankInquiry():
+        print("")
